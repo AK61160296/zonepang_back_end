8c97a0e6ef6b8c20c43a8e2b1779a6a5227ba8fa
AK61160296 <openworld1122@gmail.com>
2023-04-03 16:35:41 +0700

    feed

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -3,52 +3,30 @@ const postsRouter = express.Router();
 import { getInfinitePosts, createPostGroups } from '../controller/postsController.js';
 import path from 'path'
 import multer from "multer";
+import AWS from 'aws-sdk'
+import multerS3 from 'multer-s3'
 
-// const AWS = require('aws-sdk');
-// const multer = require('multer');
-// const multerS3 = require('multer-s3');
-// const express = require('express');
-
-// const app = express();
-// const s3 = new AWS.S3({
-//     accessKeyId: '<ACCESS KEY>',
-//     secretAccessKey: '<SECRET ACCESS KEY>',
-//     endpoint: '<SPACE ENDPOINT>',
-//     region: '<REGION>',
-//     s3ForcePathStyle: true,
-//     signatureVersion: 'v4'
-// });
-
-// const upload = multer({
-//     storage: multerS3({
-//         s3: s3,
-//         bucket: '<BUCKET NAME>',
-//         acl: 'public-read',
-//         contentType: multerS3.AUTO_CONTENT_TYPE,
-//         key: function (req, file, cb) {
-//             cb(null, Date.now().toString() + '-' + file.originalname)
-//         }
-//     })
-// });
-
-
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, path.basename(file.originalname, path.extname(file.originalname)) + '-' + Date.now() + path.extname(file.originalname));
-    }
+const app = express();
+const s3 = new AWS.S3({
+    accessKeyId: 'DO00LZHLWGBLZWNB23AB',
+    secretAccessKey: 'hGKP79CIMKB7ZJZq4wZFBSEA3EsPq6azSR1YpnfosYU',
+    endpoint: 'https://sgp1.digitaloceanspaces.com',
+    region: 'sgp1',
+    s3ForcePathStyle: true,
+    signatureVersion: 'v4'
 });
 
 const upload = multer({
-    dest: './upload/',
-    limits: {
-        fileSize: 1000000,
-    },
-    storage: storage
-})
+    storage: multerS3({
+        s3: s3,
+        bucket: 'zonepang',
+        acl: 'public-read',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: function (req, file, cb) {
+            cb(null, 'post-img/' + Date.now().toString() + '-' + file.originalname)
+        }
+    })
+});
 
 postsRouter.post('/createPostGroups', upload.any('file'), async function (req, res) {
     try {
@@ -56,12 +34,44 @@ postsRouter.post('/createPostGroups', upload.any('file'), async function (req, r
         console.log(files)
         const { content, user_id, group_id } = req.body;
         const groupIds = group_id.split(',').map(id => parseInt(id.trim())); // แปลง string ให้เป็น array ของ integer
-        const status = await createPostGroups(content, user_id, groupIds, files);
-        res.json({
-            data: status
-        });
+        const res = await createPostGroups(content, user_id, groupIds, files);
+        if (res.status === 'success') {
+            res.json({
+                data: res
+            });
+        } else {
+            files.forEach(file => {
+                s3.deleteObject({
+                    Bucket: 'zonepang',
+                    Key: file.key
+                }, function (err, data) {
+                    if (err) {
+                        console.log(err, err.stack);
+                    } else {
+                        console.log(data);
+                    }
+                });
+            });
+            res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
     } catch (error) {
-        console.log(error)
+        files.forEach(file => {
+            s3.deleteObject({
+                Bucket: 'zonepang',
+                Key: file.key
+            }, function (err, data) {
+                if (err) {
+                    console.log(err, err.stack);
+                } else {
+                    console.log(data);
+                }
+            });
+        });
+        res.status(500).json({
+            message: 'Internal server error'
+        });
     }
 });
 
@@ -91,3 +101,22 @@ postsRouter.get('/getPostComments', async function (req, res) {
 });
 
 export { postsRouter };
+
+
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, './uploads/');
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, path.basename(file.originalname, path.extname(file.originalname)) + '-' + Date.now() + path.extname(file.originalname));
+//     }
+// });
+
+// const upload = multer({
+//     dest: './upload/',
+//     limits: {
+//         fileSize: 1000000,
+//     },
+//     storage: storage
+// })
